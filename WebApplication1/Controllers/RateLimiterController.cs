626e7996b33831bcfeff7bc57d8fe60376e606f8
Abhishek Bajpai <imabhi750@gmail.com>
2026-07-21 23:32:42 +0530

    Add project files.

---
+++ WebApplication1/Controllers/RateLimiterController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RateLimiterController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Rate Limiting is working!");
+        }
+    }
+}
